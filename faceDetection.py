1eee350dbe5e425f28b51f594d9e3be5b4ecc94f
Vivek Kumar <vivek.kumar007656@gmail.com>
2020-12-22 19:16:59 +0530

    added some objet detection files

---
+++ faceDetection.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture("group_selfies.mp4")
+
+while (cap.isOpened()):
+    ret, img = cap.read()
+    # cv2.namedWindow('video', cv2.WINDOW_NORMAL)
+    # cv2.imshow('video', img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
